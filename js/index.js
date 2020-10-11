7af80e9476385d4efe2f4d9cd0b55e414b9b6723
imabotplayer <72506325+imabotplayer@users.noreply.github.com>
2020-10-11 17:18:10 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 // REGION 0: North America, REGION 1: EUROPE, REGION 3: ASIA
 let currentregion = 0;
 let regionbuttons = [document.querySelector(".region.na"), document.querySelector(".region.europe"), document.querySelector(".region.asia")];
+let regionsgms = [document.querySelector(".rg0"), document.querySelector(".rg1"), document.querySelector(".rg2")]
 
 window.addEventListener("scroll", function () {
     if (window.scrollY == 0) {
@@ -10,6 +11,27 @@ window.addEventListener("scroll", function () {
     }
 });
 
+function updateRegion(regioncode) {
+    if(regioncode == 0) {
+        regionsgms[0].setAttribute("style", "display: block;");
+        regionsgms[1].setAttribute("style", "display: none;");
+        regionsgms[2].setAttribute("style", "display: none;");
+
+    } else if(regioncode == 1) {
+        regionsgms[0].setAttribute("style", "display: none;");
+        regionsgms[1].setAttribute("style", "display: block;");
+        regionsgms[2].setAttribute("style", "display: none;");
+
+    } else if(regioncode == 2) {
+        regionsgms[0].setAttribute("style", "display: none;");
+        regionsgms[1].setAttribute("style", "display: none;");
+        regionsgms[2].setAttribute("style", "display: block;");
+
+    }
+}
+
+updateRegion(currentregion)
+
 for (let i = 0; i < regionbuttons.length; i++) {
     const element = regionbuttons[i];
     element.addEventListener("click", function () {
@@ -17,6 +39,7 @@ for (let i = 0; i < regionbuttons.length; i++) {
             regionbuttons[currentregion].classList.remove("selected");
             if(regionbuttons[i].classList.contains("selected") == false) {
                 regionbuttons[i].classList.add("selected");
+                updateRegion(i)
             }
             currentregion = i;
         }
